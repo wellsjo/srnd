81dd63e5c41e58e907e941753ac7aaddd8ae4dd7
wellsjo <wellsjohnston@gmail.com>
2014-06-29 11:26:51 -0400

    added bold, italicize. general improvements to documentation

--- surround.js
+++ surround.js
@@ -1,22 +1,32 @@
 /**
  * surround.js
  *
- * Utility function for wrapping a search term in custom tags.
+ * A set of utility functions for wrapping strings of text in various ways.
  *
- * @param string blob       The string of text to search through
- * @param string term       The term to search for in blob
- * @param string tag        The string that will wrap all instances of term in blob
- * @param object tag        If tag is an object, you can specify open and close tags
- *                          with tag.open_tag and tag.close_tag
- * @param bool   case_s     (Optional) If true, search will be case sensitive.  Default is false.
- * 
+ * The SJS object is exposed, and it comes with the following functions:
+ * SJS.surround
+ * SJS.highlight
+ * SJS.bold
+ * SJS.italicize
+ *
  * @author Wells Johnston <wellsjohnston@gmail.com>
  */
- 
 var SJS = {};
 
-SJS.surround = function(blob, term, tag, case_s) {
-    var last_index = blob.length, indeces = Array(), index, open_tag, close_tag;
+/**
+ * surround
+ *
+ * Utility function for wrapping a search term in custom tags.
+ *
+ * @param string text       The string of text to search through.
+ * @param string term       The term to search for in text.
+ * @param string tag        The string that will wrap all instances of term in text.
+ * @param object tag        If tag is an object, you can specify open and close tags
+ *                          with tag.open_tag and tag.close_tag.
+ * @param bool   case_s     (Optional) If true, search will be case sensitive.  Default is false.
+ */
+SJS.surround = function(text, term, tag, case_s) {
+    var last_index = text.length, indeces = Array(), index, open_tag, close_tag;
     if (typeof tag === 'string') {
         open_tag = tag;
         close_tag = tag;
@@ -24,41 +34,100 @@ SJS.surround = function(blob, term, tag, case_s) {
         open_tag = tag.open_tag;
         close_tag = tag.close_tag;
     }
-    index = case_s ? blob.lastIndexOf(term) 
-        : blob.toLowerCase().lastIndexOf(term.toLowerCase());
+    index = case_s ? text.lastIndexOf(term)
+        : text.toLowerCase().lastIndexOf(term.toLowerCase());
     while (index !== -1) {
-        blob = blob.substring(0, index) + open_tag
-            + blob.substring(index, index + term.length) + close_tag
-            + blob.substring(index + term.length);
-        index = case_s ? blob.lastIndexOf(term, index-1) 
-            : blob.toLowerCase().lastIndexOf(term.toLowerCase(), index - 1);
+        text = text.substring(0, index) + open_tag
+            + text.substring(index, index + term.length) + close_tag
+            + text.substring(index + term.length);
+        index = case_s ? text.lastIndexOf(term, index-1)
+            : text.toLowerCase().lastIndexOf(term.toLowerCase(), index - 1);
     }
-    return blob;
+    return text;
 }
 
-SJS.tag = function(blob, term, properties, case_s) {
+
+/**
+ * tag
+ *
+ * Shorthand for creating html elements using surround.
+ *
+ * @param string text           String of text to search through.
+ * @param string term           The search term.
+ * @param object properties     This object specifies how the html element is made.
+ *                              The element type is specified with "properties.element"
+ *                              All other attributes are key-value pairs on the properties object.
+ *                              Ex: { element: 'a', class: "btn btn-danger", hidden: null }
+ *                              will produce: <a class="btn btn-danger" hidden>{term}</a>
+ * @param bool   case_s         (Optional) Whether to use case-sensitive search.
+ *
+ */
+SJS.tag = function(text, term, properties, case_s) {
     var attributes = '';
     for (attribute in properties) {
         if (attribute !== 'element') {
-            if (properties[attribute] == false || properties[attribute] === null) {
-                attributes += attribute + " ";
+            if (properties[attribute] == false || properties[attribute] === null
+                || properties[attribute] === 'undefined' || properties[attribute] === '') {
+                attributes += attribute + ' ';
             } else {
-                attributes += attribute + '="' + properties[attribute] + ' "';    
+                attributes += attribute + '="' + properties[attribute] + '" ';
             }
-            
+
         }
     }
-    var element = {
-        open_tag: '<' + properties.element + ' ' + attributes + '>',
+   attributes = attributes.length ? ' ' + attributes.substr(0, attributes.length - 1) : '';
+    var tags = {
+        open_tag: '<' + properties.element + attributes + '>',
         close_tag: '</' + properties.element + '>'
     };
-    return SJS.surround(blob, term, element, case_s);
+    return SJS.surround(text, term, tags, case_s);
 }
 
-SJS.highlight = function(blob, term, case_s) {
-    var tag = {
-        open_tag: '<span style="background-color:yellow">',
-        close_tag: '</span>'
+/**
+ * highlight
+ *
+ * Shorthand for highlighting text yellow using surround.
+ *
+ * @param string text       The text to search through.
+ * @param string term       The search term.
+ * @param bool   case_s     Whether to use case-sensitive search.
+ */
+SJS.highlight = function(text, term, case_s) {
+    var properties = {
+        element: 'span',
+        style: 'background-color:yellow',
     }
-    return SJS.surround(blob, term, tag, case_s);
+    return SJS.tag(text, term, properties, case_s);
+}
+
+/**
+ * bold
+ *
+ * Shorthand for making text bold using surround.
+ *
+ * @param string text       The text to search through.
+ * @param string term       The search term.
+ * @param bool   case_s     Whether to use case-sensitive search.
+ */
+SJS.bold = function(text, term, case_s) {
+    var properties = {
+        element: 'strong',
+    }
+    return SJS.tag(text, term, properties, case_s);
+}
+
+/**
+ * italicize
+ *
+ * Shorthand for italicizing text using surround.
+ *
+ * @param string text       The text to search through.
+ * @param string term       The search term.
+ * @param bool   case_s     Whether to use case-sensitive search.
+ */
+SJS.italicize = function(text, term, case_s) {
+    var properties = {
+        element: 'em',
+    }
+    return SJS.tag(text, term, properties, case_s);
 }
